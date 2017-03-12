f8b6dbdd880f46332c9a829ce12b995cdf4069a0
Siddharth Sudheer <siddharthsudheer@gmail.com>
2017-03-12 06:33:38 -0400

    sign in trigger working

--- src/client/app/components/game_name/game_name.component.ts
+++ src/client/app/components/game_name/game_name.component.ts
@@ -39,5 +39,4 @@ export class GameNameComponent implements OnInit {
       }
     });
   }
-  }
 }
--- src/client/app/components/signin/signin.service.ts
+++ src/client/app/components/signin/signin.service.ts
@@ -13,11 +13,11 @@ export class SignInService {
     /*in some function or globally*/
     if(platform.device.os === platform.platformNames.ios){
       /*localhost for ios*/
-      nativePlatformLocalhost= '127.0.0.1:5000/sign-in';
+      nativePlatformLocalhost= 'http://localhost:5000/sign-in';
     }
     else if(platform.device.os === platform.platformNames.android){
       /*localhost for android*/
-      nativePlatformLocalhost= '10.0.2.2:5000/sign-in';
+      nativePlatformLocalhost= 'http://10.0.2.2:5000/sign-in';
     }
     this.http.get(nativePlatformLocalhost).map(response => response.text()).subscribe();
   }
