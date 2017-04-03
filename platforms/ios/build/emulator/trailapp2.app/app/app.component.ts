446b590ceebf8c99a540ca49e7a7466ee8b3e4b2
Siddharth Sudheer <siddharthsudheer@gmail.com>
2017-04-02 23:12:22 -0400

    Again

--- platforms/ios/build/emulator/trailapp2.app/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "tl-app",
-    templateUrl: "app.component.html",
-})
-export class AppComponent { }
---
+++ platforms/ios/build/trailapp.build/Debug-iphonesimulator/trailapp.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/siddharthsudheer/repos/computer-science/cs345/project-2-trail-app/trail-app/platforms/ios/internal/main.m \
+  /Users/siddharthsudheer/repos/computer-science/cs345/project-2-trail-app/trail-app/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/siddharthsudheer/repos/computer-science/cs345/project-2-trail-app/trail-app/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/siddharthsudheer/repos/computer-science/cs345/project-2-trail-app/trail-app/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/siddharthsudheer/repos/computer-science/cs345/project-2-trail-app/trail-app/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/siddharthsudheer/repos/computer-science/cs345/project-2-trail-app/trail-app/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/siddharthsudheer/repos/computer-science/cs345/project-2-trail-app/trail-app/platforms/ios/internal/TNSDebugging.h
