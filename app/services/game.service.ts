ffc2d156777a72df248d69575fd3b65ca82e49cc
Siddharth Sudheer <siddharthsudheer@gmail.com>
2017-04-09 23:04:07 -0400

    Game.services scaffolding

--- app/services/game.service.ts
+++ app/services/game.service.ts
@@ -9,5 +9,47 @@ export class GameService {
 
     constructor() {}
 
+// def get_all_locations():
+// 	pass
+//
+//
+// def create_new_game(user_id, game_name):
+// 	pass
+//
+//
+// def add_invitees_to_game(user_id, game_name, invitees_list):
+// 	pass
+//
+//
+// def get_all_created_games(user_id):
+// 	pass
+//
+//
+// def get_invited_games(user_id):
+// 	pass
+//
+//
+// def get_game_invitees(user_id, game_name):
+// 	pass
+//
+//
+// def get_game_attributes(player_id, creator_id, game_name):
+// 	pass
+//
+//
+// def get_completed_games(user_id):
+// 	pass
+//
+//
+// def update_game_status(player_id, creator_id, game_name, time):
+// 	pass
+//
+//
+// def delete_game_admin(user_id, game_name):
+// 	pass
+//
+//
+// def delete_game_invitee(player_id, creator_id, game_name):
+// 	pass
 }
 
