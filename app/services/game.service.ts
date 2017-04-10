07ce5c862103086f3db06a8bfe54663bc6ac8779
Lam Nguyen <ltnguyen14@earlham.edu>
2017-04-10 00:48:02 -0400

    game services

--- app/services/game.service.ts
+++ app/services/game.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { RestService } from "../services";
 import { Http, Headers, Response, RequestOptions } from "@angular/http";
 import { Observable as RxObservable } from "rxjs/Rx";
 import "rxjs/add/operator/map";
@@ -7,49 +8,58 @@ import "rxjs/add/operator/map";
 @Injectable()
 export class GameService {
     public locations:JSON;
-    
-    constructor() {}
 
-// def get_all_locations():
-// 	pass
-//
-//
-// def create_new_game(user_id, game_name):
-// 	pass
-//
-//
-// def add_invitees_to_game(user_id, game_name, invitees_list):
-// 	pass
-//
-//
-// def get_all_created_games(user_id):
-// 	pass
-//
-//
-// def get_invited_games(user_id):
-// 	pass
-//
-//
-// def get_game_invitees(user_id, game_name):
-// 	pass
-//
-//
-// def get_game_attributes(player_id, creator_id, game_name):
-// 	pass
-//
-//
-// def get_completed_games(user_id):
-// 	pass
-//
-//
-// def update_game_status(player_id, creator_id, game_name, time):
-// 	pass
-//
-//
-// def delete_game_admin(user_id, game_name):
-// 	pass
-//
-//
-// def delete_game_invitee(player_id, creator_id, game_name):
-// 	pass
+    constructor (private RestService:RestService) {}
+
+    public get_locations(user_id){
+      this.RestService
+              .post({ username: user_id }, "get_locations");
+    }
+
+    public create_new_game(user_id, game_name){
+      this.RestService
+              .post({ user_id: user_id, game_name: game_name }, "create_new_game");
+    }
+
+    public add_invitees_to_game(user_id, game_name, invitees_list){
+      this.RestService
+              .post({ user_id: user_id, game_name: game_name , invitees_list:invitees_list},
+                          "add_invitees_to_game");
+    }
+
+    public get_all_created_games(user_id){
+      this.RestService
+              .post({ user_id: user_id }, "get_all_created_games");
+    }
+
+    public get_game_invitees(user_id, game_name){
+      this.RestService
+              .post({ user_id: user_id, game_name: game_name }, "get_game_invitees");
+    }
+
+    public get_game_attributes(player_id, creator_id, game_name){
+      this.RestService
+              .post({ player_id: player_id, creator_id:creator_id, game_name:game_name}, "get_game_attributes");
+    }
+
+    public get_completed_games(user_id){
+      this.RestService
+              .post({ user_id: user_id }, "get_completed_games");
+    }
+
+    public update_game_status(player_id, creator_id, game_name, time){
+      this.RestService
+              .post({ player_id:player_id, creator_id:creator_id, game_name:game_name, time:time }, "update_game_status");
+    }
+
+    public delete_game_admin(user_id, game_name){
+      this.RestService
+              .post({ user_id: user_id, game_name:game_name }, "delete_game_admin");
+    }
+
+    public delete_game_invitee(player_id, creator_id, game_name){
+      this.RestService
+              .post({ player_id: player_id, creator_id:creator_id, game_name:game_name }, "delete_game_invitee");      
+    }
+
 }
